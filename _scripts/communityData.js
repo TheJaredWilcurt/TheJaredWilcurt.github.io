85a3f2f1032fbde2e99ef3891583288897414df2
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2017-06-08 16:36:51 -0400

    Create communityData.js

--- _scripts/communityData.js
+++ _scripts/communityData.js
@@ -2,7 +2,7 @@
 var communityData = [
     {
         'name': 'Project Sunday',
-        'description': '',
+        'description': 'A private group I co-founded that meets every Sunday to focus on working on personal projects and learning from each other. This group is responsible for getting several friends the knowledge and experience to obtain developer positions.',
         'role': [
             'Co-Founder',
             'Organizer',
@@ -13,21 +13,57 @@ var communityData = [
     },
     {
         'name': 'Free Code Camp Indy',
-        'description': '',
+        'description': 'This is a local, public Meetup group that focuses on extended learning outside of the FreeCodeCamp.com curriculum. We hold hackathons, monthly talks, and code \'n coffee meet ups. Myself and Gwen Faraday run the Indianapolis group. I\'ve given several talks on the group\'s most requested material (Node, NPM, Bower, Grunt/Gulp, Sass/CSS).',
         'role': [
             'Organizer',
             'Member',
-            'Mentor'
+            'Mentor',
+            'Presenter'
         ],
         'time': '1 year'
     },
     {
         'name': 'Coder Dojo Indy',
-        'description': '',
+        'description': 'Coder Dojo is a free club for kids under 18 to join learn to program. I\'ve been volunteering as a mentor for 2 years.',
         'role': [
             'Mentor'
         ],
         'time': '2 years'
+    },
+    {
+        'name': 'Indy.JS',
+        'description': 'Indy.JS is the largest JavaScript meetup in Indianapolis. I\'ve been regularly attending this meetup for years and have given several talks there.',
+        'role': [
+            'Member',
+            'Presenter'
+        ],
+        'time': '3 years'
+    },
+    {
+        'name': 'Node.JS',
+        'description': 'A general-purpose Node.JS meetup group that covers everything from Server Side best practices to Node-Bots. I routinely come to this meetup and have given small talks on Node, NPM, ',
+        'role': [
+            'Member',
+            'Presenter'
+        ],
+        'time': '2 years'
+    },
+    {
+        'name': 'Open Indy Brigade',
+        'description': 'Open Indy Brigade is a bridge between the Tech side of Indianapolis and the Civic/Political side. It focuses on solving Civic problems with technology. Developers, UI/UX designers, and Project Managers are presented with the problems our city faces and come up with practical solutions.',
+        'role': [
+            'Member'
+        ],
+        'time': '1.5 years'
+    },
+    {
+        'name': 'Civic UX Group',
+        'description': 'This group was created as part of the Open Indy Brigade and has it\'s longest running group. I currently run this group which focuses on solving Civic UX issues with a group of dedicated volunteers. Our most successful project is Koa11y, a desktop app created to lower the technical barrier of entry for finding accessibility issues on the web.',
+        'role': [
+            'Organizer',
+            'Mentor'
+        ],
+        'time': '1.5 years'
     }/*,
     {
         'name': '',
@@ -36,7 +72,8 @@ var communityData = [
             'Co-Founder',
             'Organizer',
             'Member',
-            'Mentor'
+            'Mentor',
+            'Presenter'
         ],
         'time': ''
     }
