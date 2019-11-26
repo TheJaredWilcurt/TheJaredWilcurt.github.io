9c987f1ff9508d0900b12198a149f491d3b8c360
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-11-26 11:42:13 -0500

    Lint

--- _scripts/main.js
+++ _scripts/main.js
@@ -7,28 +7,28 @@
  * @return {object}      Object comprised of tech and roles subobjects. Each lists k:v pairs of tech/role and how many times it's used.
  */
 function listOfTechnologiesAndRoles (data, obj) {
-    obj = obj || {
-        tech: {},
-        roles: {}
-    };
-    data.forEach(function (item) {
-        if (item.tech && item.tech.length > 0) {
-            item.tech.forEach(function (technology) {
-                obj.tech[technology] = obj.tech[technology] + 1 || 1;
-            });
-        }
-        if (item.roles && item.roles.length > 0) {
-            item.roles.forEach(function (role) {
-                obj.roles[role] = obj.roles[role] + 1 || 1;
-            });
-        }
-    });
-    return obj;
+  obj = obj || {
+    tech: {},
+    roles: {}
+  };
+  data.forEach(function (item) {
+    if (item.tech && item.tech.length > 0) {
+      item.tech.forEach(function (technology) {
+        obj.tech[technology] = obj.tech[technology] + 1 || 1;
+      });
+    }
+    if (item.roles && item.roles.length > 0) {
+      item.roles.forEach(function (role) {
+        obj.roles[role] = obj.roles[role] + 1 || 1;
+      });
+    }
+  });
+  return obj;
 }
 
 // eslint-disable-next-line no-unused-vars
 function requestFailed () {
-    return 'There was an error in making the network request. Please refresh the page. If the issue continues, <a href="http://github.com/TheJaredWilcurt/TheJaredWilcurt.github.io/issues">report it here</a>.';
+  return 'There was an error in making the network request. Please refresh the page. If the issue continues, <a href="http://github.com/TheJaredWilcurt/TheJaredWilcurt.github.io/issues">report it here</a>.';
 }
 
 window.techAndRolesStats = listOfTechnologiesAndRoles([]);
@@ -36,249 +36,249 @@ window.techAndRolesStats = listOfTechnologiesAndRoles([]);
 console.log(window.techAndRolesStats);
 
 // eslint-disable-next-line no-unused-vars
-var projectsHighlight = new Vue({
-    el: '#projects-highlight',
-    data: {
-        projects: [],
-        networkRequestFailed: false
-    }
+const projectsHighlight = new Vue({
+  el: '#projects-highlight',
+  data: {
+    projects: [],
+    networkRequestFailed: false
+  }
 });
 
 // eslint-disable-next-line no-unused-vars
-var otherProjects = new Vue({
-    el: '#other-projects',
-    data: {
-        projects: [],
-        typesChecked: {},
-        showDetails: false,
-        networkRequestFailed: false
+const otherProjects = new Vue({
+  el: '#other-projects',
+  data: {
+    projects: [],
+    typesChecked: {},
+    showDetails: false,
+    networkRequestFailed: false
+  },
+  methods: {
+    detailsClassToggle: function (project) {
+      const detailsClass = 'hide-description';
+      if (this.showDetails || project.showDetails) {
+        detailsClass = 'show-description';
+      }
+      return detailsClass;
     },
-    methods: {
-        detailsClassToggle: function (project) {
-            var detailsClass = 'hide-description';
-            if (this.showDetails || project.showDetails) {
-                detailsClass = 'show-description';
-            }
-            return detailsClass;
-        },
-        toggleCurrentDetails: function (project) {
-            project.showDetails = !project.showDetails;
-        },
-        createTypesChecked: function () {
-            var allTypes = {};
-            this.projects.forEach(function (project) {
-                if (typeof(project.type) === 'string') {
-                    allTypes[project.type] = true;
-                }
-            });
-            this.typesChecked = allTypes;
-        },
-        setAllDetailsToHidden: function () {
-            this.projects.forEach(function (project) {
-                Vue.set(project, 'showDetails', false);
-            });
-        }
+    toggleCurrentDetails: function (project) {
+      project.showDetails = !project.showDetails;
     },
-    computed: {
-        allProjectTypes: function () {
-            var allTypes = [];
-            this.projects.forEach(function (project) {
-                if (typeof(project.type) === 'string') {
-                    allTypes.push(project.type);
-                }
-            });
-            var uniqueArray = allTypes.filter(function (item, pos) {
-                return allTypes.indexOf(item) === pos;
-            });
-            return uniqueArray;
-        },
-        filteredProjects: function () {
-            return this.projects.filter(function (project) {
-                return this.typesChecked[project.type];
-            }.bind(this));
-        },
-        typesCount: function () {
-            var typeCounts = {};
-            this.projects.forEach(function (project) {
-                if (!project.major) {
-                    typeCounts[project.type] = typeCounts[project.type] + 1 || 1;
-                }
-            });
-
-            return typeCounts;
+    createTypesChecked: function () {
+      const allTypes = {};
+      this.projects.forEach(function (project) {
+        if (typeof(project.type) === 'string') {
+          allTypes[project.type] = true;
         }
+      });
+      this.typesChecked = allTypes;
+    },
+    setAllDetailsToHidden: function () {
+      this.projects.forEach(function (project) {
+        Vue.set(project, 'showDetails', false);
+      });
     }
+  },
+  computed: {
+    allProjectTypes: function () {
+      const allTypes = [];
+      this.projects.forEach(function (project) {
+        if (typeof(project.type) === 'string') {
+          allTypes.push(project.type);
+        }
+      });
+      const uniqueArray = allTypes.filter(function (item, pos) {
+        return allTypes.indexOf(item) === pos;
+      });
+      return uniqueArray;
+    },
+    filteredProjects: function () {
+      return this.projects.filter(function (project) {
+        return this.typesChecked[project.type];
+      }.bind(this));
+    },
+    typesCount: function () {
+      const typeCounts = {};
+      this.projects.forEach(function (project) {
+        if (!project.major) {
+          typeCounts[project.type] = typeCounts[project.type] + 1 || 1;
+        }
+      });
+
+      return typeCounts;
+    }
+  }
 });
 
 axios.get('/_scripts/data/projects.json')
-    .then(function (response) {
-        projectsHighlight.projects = response.data;
-        otherProjects.projects = response.data;
-        otherProjects.setAllDetailsToHidden();
-        otherProjects.createTypesChecked();
+  .then(function (response) {
+    projectsHighlight.projects = response.data;
+    otherProjects.projects = response.data;
+    otherProjects.setAllDetailsToHidden();
+    otherProjects.createTypesChecked();
+    window.techAndRolesStats = listOfTechnologiesAndRoles(response.data, window.techAndRolesStats);
+  }.bind(this))
+  .catch(function (err) {
+    projectsHighlight.networkRequestFailed = true;
+    otherProjects.networkRequestFailed = true;
+    // eslint-disable-next-line no-console
+    console.log(err);
+  });
+
+// eslint-disable-next-line no-unused-vars
+const community = new Vue({
+  el: '#community',
+  data: {
+    groups: [],
+    networkRequestFailed: false
+  },
+  methods: {
+    toggleCurrentDetails: function (group) {
+      group.showDetails = !group.showDetails;
+    },
+    showAllDetails: function () {
+      this.groups.forEach(function (group) {
+        group.showDetails = true;
+      });
+    },
+    setAllDetailsToHidden: function () {
+      this.groups.forEach(function (group) {
+        Vue.set(group, 'showDetails', false);
+      });
+    }
+  },
+  mounted: function () {
+    axios.get('/_scripts/data/community.json')
+      .then(function (response) {
+        this.groups = response.data;
+        this.setAllDetailsToHidden();
         window.techAndRolesStats = listOfTechnologiesAndRoles(response.data, window.techAndRolesStats);
-    }.bind(this))
-    .catch(function (err) {
-        projectsHighlight.networkRequestFailed = true;
-        otherProjects.networkRequestFailed = true;
+      }.bind(this))
+      .catch(function (err) {
+        this.networkRequestFailed = true;
         // eslint-disable-next-line no-console
         console.log(err);
-    });
-
-// eslint-disable-next-line no-unused-vars
-var community = new Vue({
-    el: '#community',
-    data: {
-        groups: [],
-        networkRequestFailed: false
-    },
-    methods: {
-        toggleCurrentDetails: function (group) {
-            group.showDetails = !group.showDetails;
-        },
-        showAllDetails: function () {
-            this.groups.forEach(function (group) {
-                group.showDetails = true;
-            });
-        },
-        setAllDetailsToHidden: function () {
-            this.groups.forEach(function (group) {
-                Vue.set(group, 'showDetails', false);
-            });
-        }
-    },
-    mounted: function () {
-        axios.get('/_scripts/data/community.json')
-            .then(function (response) {
-                this.groups = response.data;
-                this.setAllDetailsToHidden();
-                window.techAndRolesStats = listOfTechnologiesAndRoles(response.data, window.techAndRolesStats);
-            }.bind(this))
-            .catch(function (err) {
-                this.networkRequestFailed = true;
-                // eslint-disable-next-line no-console
-                console.log(err);
-            }.bind(this));
-    }
+      }.bind(this));
+  }
 });
 
 // eslint-disable-next-line no-unused-vars
-var talks = new Vue({
-    el: '#talks',
-    data: {
-        talks: [],
-        showDetails: false,
-        networkRequestFailed: false
+const talks = new Vue({
+  el: '#talks',
+  data: {
+    talks: [],
+    showDetails: false,
+    networkRequestFailed: false
+  },
+  methods: {
+    toggleCurrentDetails: function (talk) {
+      talk.showDetails = !talk.showDetails;
     },
-    methods: {
-        toggleCurrentDetails: function (talk) {
-            talk.showDetails = !talk.showDetails;
-        },
-        detailsClassToggle: function () {
-            var detailsClass = 'hide-description';
-            if (this.showDetails) {
-                detailsClass = 'show-description';
-            }
-            return detailsClass;
-        },
-        setAllDetailsToHidden: function () {
-            this.talks.forEach(function (talk) {
-                Vue.set(talk, 'showDetails', false);
-            });
-        }
+    detailsClassToggle: function () {
+      const detailsClass = 'hide-description';
+      if (this.showDetails) {
+        detailsClass = 'show-description';
+      }
+      return detailsClass;
     },
-    mounted: function () {
-        axios.get('/_scripts/data/talks.json')
-            .then(function (response) {
-                this.talks = response.data;
-                this.setAllDetailsToHidden();
-                window.techAndRolesStats = listOfTechnologiesAndRoles(response.data, window.techAndRolesStats);
-            }.bind(this))
-            .catch(function (err) {
-                this.networkRequestFailed = true;
-                // eslint-disable-next-line no-console
-                console.log(err);
-            }.bind(this));
+    setAllDetailsToHidden: function () {
+      this.talks.forEach(function (talk) {
+        Vue.set(talk, 'showDetails', false);
+      });
     }
+  },
+  mounted: function () {
+    axios.get('/_scripts/data/talks.json')
+      .then(function (response) {
+        this.talks = response.data;
+        this.setAllDetailsToHidden();
+        window.techAndRolesStats = listOfTechnologiesAndRoles(response.data, window.techAndRolesStats);
+      }.bind(this))
+      .catch(function (err) {
+        this.networkRequestFailed = true;
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }.bind(this));
+  }
 });
 
 // eslint-disable-next-line no-unused-vars
-var quotes = new Vue({
-    el: '#quotes',
-    data: {
-        quotes: [],
-        networkRequestFailed: false
-    },
-    methods: {},
-    mounted: function () {
-        axios.get('/_scripts/data/quotes.json')
-            .then(function (response) {
-                this.quotes = response.data;
-            }.bind(this))
-            .catch(function (err) {
-                this.networkRequestFailed = true;
-                // eslint-disable-next-line no-console
-                console.log(err);
-            }.bind(this));
-    }
+const quotes = new Vue({
+  el: '#quotes',
+  data: {
+    quotes: [],
+    networkRequestFailed: false
+  },
+  methods: {},
+  mounted: function () {
+    axios.get('/_scripts/data/quotes.json')
+      .then(function (response) {
+        this.quotes = response.data;
+      }.bind(this))
+      .catch(function (err) {
+        this.networkRequestFailed = true;
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }.bind(this));
+  }
 });
 
 // eslint-disable-next-line no-unused-vars
-var footer = new Vue({
-    el: 'footer',
-    data: {
-        addr: '',
-        to: 'mailto:0@0.0'
+const footer = new Vue({
+  el: 'footer',
+  data: {
+    addr: '',
+    to: 'mailto:0@0.0'
+  },
+  methods: {
+    changeTo: function () {
+      this.to = this.ROT47('>2:=E@i%96y2C65(:=4FCEo8>2:=]4@>');
     },
-    methods: {
-        changeTo: function () {
-            this.to = this.ROT47('>2:=E@i%96y2C65(:=4FCEo8>2:=]4@>');
-        },
-        /**
-         * Takes a charCode  and rotates 47 characters and returns the new charCode
-         * @param  {number} charCode The numeric version of a character, 97 would be for 'a'
-         * @return {number}          The new charCode after rotation
-         */
-        ROT47CharCode: function (charCode) {
-            if (typeof(charCode) === 'number') {
-                // '!' == 33; 'O' == 79
-                if (charCode >= 33 && charCode <= 79) {
-                    charCode = charCode + 47;
-                // 'P' == 80; '~' == 126
-                } else if (charCode >= 80 && charCode <= 126) {
-                    charCode = charCode - 47;
-                }
-            }
-
-            return charCode;
-        },
-        /**
-         * Loops over the characters in a string, returns a ROT47 version of them
-         * @param  {string} str Any string of text.
-         * @return {string}     The rotated string, ready for use.
-         */
-        ROT47: function (str) {
-            var newString = [];
-
-            for (var i = 0; i < str.length; i++) {
-                // a => 97
-                var char = str.charCodeAt(i);
-                // 97 => 50
-                var newChar = this.ROT47CharCode(char);
-                // 50 => '2'
-                newChar = String.fromCharCode(newChar);
-                // [] => ['2']
-                newString.push(newChar);
-            }
-
-            // ['2'] => '2'
-            newString = newString.join('');
-
-            // '2'
-            return newString;
+    /**
+     * Takes a charCode  and rotates 47 characters and returns the new charCode
+     * @param  {number} charCode The numeric version of a character, 97 would be for 'a'
+     * @return {number}          The new charCode after rotation
+     */
+    ROT47CharCode: function (charCode) {
+      if (typeof(charCode) === 'number') {
+        // '!' == 33; 'O' == 79
+        if (charCode >= 33 && charCode <= 79) {
+          charCode = charCode + 47;
+        // 'P' == 80; '~' == 126
+        } else if (charCode >= 80 && charCode <= 126) {
+          charCode = charCode - 47;
         }
+      }
+
+      return charCode;
     },
-    created: function () {
-        this.addr = this.ROT47('>@4]=:2>8oECF4=:(56C2y69%').replace('@', '<span class="hide">kitten</span>@');
+    /**
+     * Loops over the characters in a string, returns a ROT47 version of them
+     * @param  {string} str Any string of text.
+     * @return {string}     The rotated string, ready for use.
+     */
+    ROT47: function (str) {
+      const newString = [];
+
+      for (const i = 0; i < str.length; i++) {
+        // a => 97
+        const char = str.charCodeAt(i);
+        // 97 => 50
+        const newChar = this.ROT47CharCode(char);
+        // 50 => '2'
+        newChar = String.fromCharCode(newChar);
+        // [] => ['2']
+        newString.push(newChar);
+      }
+
+      // ['2'] => '2'
+      newString = newString.join('');
+
+      // '2'
+      return newString;
     }
+  },
+  created: function () {
+    this.addr = this.ROT47('>@4]=:2>8oECF4=:(56C2y69%').replace('@', '<span class="hide">kitten</span>@');
+  }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,92 +3,92 @@
 
 // Includes - Defining what will be used below.
 // These are pulled in from the node_modules folder.
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var sassLint = require('gulp-sass-lint');
-var crlf = require('gulp-line-ending-corrector');
+const gulp = require('gulp');
+const livereload = require('gulp-livereload');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sassLint = require('gulp-sass-lint');
+const crlf = require('gulp-line-ending-corrector');
 
 // Create expanded and .min versions of Sass styles in the _styles folder as CSS
 gulp.task('sass', function () {
-    gulp.src('_sass/style.scss')
-        .pipe(rename('style.min.css'))
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(crlf({eolc:'CRLF', encoding:'utf8'}))
-        .pipe(gulp.dest('_styles/'))
-        .pipe(livereload());
+  gulp.src('_sass/style.scss')
+    .pipe(rename('style.min.css'))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(crlf({ eolc:'CRLF', encoding:'utf8' }))
+    .pipe(gulp.dest('_styles/'))
+    .pipe(livereload());
 });
 
 // Create expanded and .min versions of Sass styles in the _styles folder as CSS
 gulp.task('sassfont', function () {
-    gulp.src('_sass/fonts.scss')
-        .pipe(rename('fonts.min.css'))
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(crlf({eolc:'CRLF', encoding:'utf8'}))
-        .pipe(gulp.dest('_styles/'))
-        .pipe(livereload());
+  gulp.src('_sass/fonts.scss')
+    .pipe(rename('fonts.min.css'))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(crlf({ eolc:'CRLF', encoding:'utf8' }))
+    .pipe(gulp.dest('_styles/'))
+    .pipe(livereload());
 });
 
 gulp.task('sasslint', function () {
-    gulp.src(['_sass/*.scss', '_sass/*.sass'])
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError());
+  gulp.src(['_sass/*.scss', '_sass/*.sass'])
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
 });
 
 // Run a local server on port 8000
 gulp.task('serve', function (done) {
-    var express = require('express');
-    var app = express();
-    //path to the folder that will be served. __dirname is project root
-    var path = __dirname;
-    app.use(express.static(path));
-    app.listen(8000, function () {
-        done();
-    });
+  const express = require('express');
+  const app = express();
+  // path to the folder that will be served. __dirname is project root
+  const path = __dirname;
+  app.use(express.static(path));
+  app.listen(8000, function () {
+    done();
+  });
 });
 
 gulp.task('refreshsass', ['sass', 'sassfont'], function () {
-    gulp.src('*.css')
-        .pipe(livereload());
+  gulp.src('*.css')
+    .pipe(livereload());
 });
 
 gulp.task('refreshjs', function () {
-    gulp.src('*.js')
-        .pipe(livereload());
+  gulp.src('*.js')
+    .pipe(livereload());
 });
 
 // Enable live reload listening from HTML files in the browser
 // if you have the LiveReload browser extension installed.
 gulp.task('refreshhtml', function () {
-    gulp.src('*.html')
-        .pipe(livereload());
+  gulp.src('*.html')
+    .pipe(livereload());
 });
 
 // Watch for changes in JS, Sass, and HTML files,
 // Uglify, Process the Sass, and reload the browser automatically
 gulp.task('watch', function () {
-    gulp.watch(['_scripts/*.js', '!_scripts/all.js'], ['refreshjs']);
-    gulp.watch('_sass/*', ['refreshsass']);
-    gulp.watch('*.html', ['refreshhtml']);
+  gulp.watch(['_scripts/*.js', '!_scripts/all.js'], ['refreshjs']);
+  gulp.watch('_sass/*', ['refreshsass']);
+  gulp.watch('*.html', ['refreshhtml']);
 
-    livereload.listen();
+  livereload.listen();
 });
 
 // Automatically opens the local server in your default browser
 gulp.task('open', function () {
-    var url = 'http://localhost:8000';
-    var OS = process.platform;
-    var executable = '';
+  const url = 'http://localhost:8000';
+  const OS = process.platform;
+  const executable = '';
 
-    //OS Specific values for opening files.
-    if (OS == 'darwin') { executable = 'open "' + url + '"'; }
-    if (OS == 'linux')  { executable = 'xdg-open ' + url;    }
-    if (OS == 'win32')  { executable = 'explorer ' + url;    }
+  // OS Specific values for opening files.
+  if (OS == 'darwin') { executable = 'open "' + url + '"'; }
+  if (OS == 'linux')  { executable = 'xdg-open ' + url;    }
+  if (OS == 'win32')  { executable = 'explorer ' + url;    }
 
-    //Run the OS specific command to open the url in the default browser
-    require('child_process').exec(executable);
+  // Run the OS specific command to open the url in the default browser
+  require('child_process').exec(executable);
 });
 
 // The default Gulp task that happens when you run gulp.
