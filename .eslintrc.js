d71187e48bccaa335be1621cd3f3fe9fe7491a85
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2021-05-24 08:32:26 -0400

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    'parserOptions': {
-        'ecmaVersion': 2015,
-        'sourceType': 'module'
-    },
-    "extends": [
-        "tjw-base"
-    ]
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: 'module'
+  },
+  extends: [
+    'tjw-base'
+  ],
+  globals: {
+    Map: true
+  }
 };
